725b2519110051f68f4e0fdb8d9db1e8c5e7f272
Nicholas Grasley <nicholas@grasley.com>
2019-01-24 11:03:47 -0700

    Added Amanda's old code and some updates to TrainingData

--- TrainingData.py
+++ TrainingData.py
@@ -4,10 +4,26 @@ Created on Wed Jan 23 17:42:38 2019
 
 @author: ngrasley
 """
-
+import pandas as pd
 class TrainingData():
+    '''This class will handle the creation of training data for record linking
+       ML models.
+    '''
     def __init__(self, state):
-        self.c1910 = pd.read_stata(r'R:/JoePriceResearch/record_linking/)
+        '''Initialize TrainingData with the raw census data
+        Inputs:
+            state -- The state in the census that you want to consider as a
+                     subsample. FIXME allow selecting multiple states
+        '''
+        self.data_direc = "R:/JoePriceResearch/record_linking/data"
+        self.c1910 = pd.read_stata("{}/census_1910/data/state_files/{}".format(data_direc,
+                                                                               state.title()))
+        self.c1920 = pd.read_stata("{}/census_1920/data/state_files/{}".format(data_direc,
+                                                                               state.lower()))
+        self.columns = self._common_elements(self.c1910.columns,
+                                             self.c1920.columns)
+
+
     
     def process_census_files(self):
         '''Choose columns to keep and generate some features
@@ -36,6 +52,8 @@ class TrainingData():
     def match(self):
         '''Generate boolean for whether censuses match.
         '''
+    def _common_elements(self, list1, list2):
+        return list(set(list1).intersection(set(list2)))
 
 class Models():
     def __init__(self):
