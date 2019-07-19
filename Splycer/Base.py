077ac7983e04e67f1f8b9230f526ed1134cf1df3
nickgrasley <32945379+nickgrasley@users.noreply.github.com>
2019-07-19 16:55:40 -0600

    update base and test_base

--- Splycer/Base.py
+++ Splycer/Base.py
@@ -6,7 +6,9 @@ Created on Wed Jul 17 20:13:29 2019
 @author: thegrasley
 """
 import numpy as np
+import pandas as pd
 from scipy.sparse import csr_matrix
+import pyodbc
 import abc
 
 class RecordBase(object, metaclass=abc.ABCMeta):
@@ -16,6 +18,7 @@ class RecordBase(object, metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def __get_item__(self, uid):
         pass
+
 class CompareBase(object, metaclass=abc.ABCMeta):
     def __init__(self, shape):
         self.shape = shape
@@ -35,7 +38,7 @@ class RecordDict(dict, RecordBase):
        at a constant rate with the number of records, this object is most
        efficient when you merely have to grab record information.
     """
-    def __init__(self,record_id, uids, features):
+    def __init__(self, record_id, uids, features):
         self.record_id = record_id
         super().__init__(zip(uids, features))
     def get_record(self, uid):
@@ -46,8 +49,22 @@ class RecordDB(RecordBase): #FIXME implement this
        sql handles a lot of the hard work of building data structures for efficient
        merges. You have to pay the upfront cost of setting up the database though.
     """
-    def __init__(self, dsn):
-        pass
+    def __init__(self, record_id, table_name, idx_name, conn_str, var_list=None):
+        self.record_id = record_id
+        self.table_name = table_name
+        self.idx_name = idx_name
+        dsn = pyodbc.connect(conn_str)
+        self.cursor = dsn.cursor()
+    
+    def __get_item__(self, uid):
+        return self.get_record(uid)
+    
+    def get_record(self, uid):
+        if self.var_list is None:
+            data = self.cursor.execute(f"select * from {self.table_name} where {self.idx_name} = {uid}").fetchall()
+        else:
+            data = self.cursor.execute(f"select {self.var_list} from {self.table_name} where {self.idx_name} = {uid}").fetchall()
+        return data
 
 class RecordDataFrame(RecordBase):
     """Records are stored in a Pandas DataFrame. This is best for small datasets
@@ -55,7 +72,13 @@ class RecordDataFrame(RecordBase):
        want to set up a sql server. However, it has slow lookup, slow merges, and
        is a memory hog, so don't use this for large datasets.
     """
-    pass
+    def __init__(self, record_id, records, uid_col):
+        self.record_id = record_id
+        self.df = pd.DataFrame(records, index_col = uid_col)
+    def __get_item__(self, uid):
+        self.df.loc[uid, :]
+    def get_record(self, uid):
+        self.df.loc[uid, :]
 
 class CompareCSR(CompareBase):
     """Comparisons are stored in a Compressed Sparse Row matrix. This is optimal
@@ -75,6 +98,20 @@ class CompareCSR(CompareBase):
         for i in range(np.shape(self.indptr)[0]-1):
             for j in range(self.indptr[i], self.indptr[i+1]):
                 yield (i, self.indices[j], self.data[j:j+1]) #this returns data as a view to allow modification of it.
+                
+    def __getitem__(self, uids):
+        uid1, uid2 = uids
+        uid1_start = self.indptr[uid1]
+        uid1_end = self.indptr[uid1 + 1]
+        i = 0
+        uid2_tmp = -1
+        while i < (uid1_end - uid1_start):
+            uid2_tmp = self.indices[uid1_start + i]
+            i += 1
+            if uid2_tmp == uid2:
+                return self.data[i]
+        return np.nan
+    
     def get_pairs(self):
         for i in range(np.shape(self.indptr)[0]-1):
             for j in range(self.indptr[i], self.indptr[i+1]):
--- Splycer/tests/test_Base.py
+++ Splycer/tests/test_Base.py
@@ -4,12 +4,53 @@ Created on Thu Jul 18 16:15:11 2019
 
 @author: ngrasley
 """
-from Splycer.Base import RecordDict
+from Splycer.Base import RecordDict, CompareCSR
 import numpy as np
 def test_RecordDict():
     rec_id = 1
-    uids = np.arange(300, dtype=np.uint32)
-    features = [np.arange(i, i + 20) for i in range(300)]
+    uids = np.arange(30, dtype=np.uint32)
+    features = [np.arange(i, i + 20) for i in range(30)]
     rec_dict = RecordDict(rec_id, uids, features)
-    for i in range(300):
+    for i in range(30):
         assert np.all(rec_dict.get_record(i) == np.arange(i, i + 20))
+
+def build_CompareCSR():
+    rec_id1 = 1
+    rec_id2 = 2
+    uids1 = np.array([0,1,1,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,6,6,7,7])
+    uids2 = np.arange(30, dtype=np.uint32)
+    data = np.tile([1,2], 15)
+    return CompareCSR(rec_id1, rec_id2, uids1, uids2, data)
+
+def test_CompareCSR_getitem():
+    comp_csr = build_CompareCSR()
+    assert comp_csr[5, 20] == 1
+    assert comp_csr[5, 17] == 2
+    assert np.isnan(comp_csr[5, 22])
+    assert np.isnan(comp_csr[5, 13])
+
+def test_CompareCSR_data():
+    comp_csr = build_CompareCSR()
+    assert comp_csr.record_id1 == 1
+    assert comp_csr.record_id2 == 2
+    assert np.all(comp_csr.indptr == np.array([0,1,3,6,10,15,21,28,30]))
+    assert np.all(comp_csr.indices == np.arange(30))
+    assert np.all(comp_csr.data == np.tile([1,2], 15))
+
+def test_CompareCSR_iter():
+    comp_csr = build_CompareCSR()
+    test_i = 0
+    test_j = 0
+    test_k = 1
+    it = 0
+    for i, j, k in comp_csr:
+        assert i == test_i
+        assert j == test_j
+        assert k[0] == test_k
+        if it == test_i:
+            test_i += 1
+            it = -1
+        test_j += 1
+        test_k = (k[0] * 2) % 3
+        it += 1
+    
