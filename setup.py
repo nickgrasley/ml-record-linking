09d1c268f85f48b94c52150319ad311ee799fafd
nickgrasley <32945379+nickgrasley@users.noreply.github.com>
2019-08-12 15:37:31 -0600

    fix environment issues

--- setup.py
+++ setup.py
@@ -6,14 +6,22 @@ Created on Mon Feb  4 15:46:53 2019
 @author: thegrasley
 """
 
-from distutils.core import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
-setup(name="Splycer",
-      version="0.1dev",
-      packages=["Splycer",],
-      ext_modules=cythonize("Splycer/*.pyx"),
+ext_modules=[
+             Extension("splycer.base", ["splycer/base.pyx"]),
+             Extension("splycer.comparisons", ["splycer/comparisons.pyx"]),
+             Extension("splycer.feature_engineer", ["splycer/feature_engineer.pyx"]),
+             Extension("splycer.pairs_set", ["splycer/pairs_set.pyx"]),
+             Extension("splycer.record_set", ["splycer/record_set.pyx"]),
+             Extension("splycer.xgboost_match", ["splycer/xgboost_match.pyx"])]
+
+setup(name="splycer",
+      version="0.1.2",
+      packages=["splycer",],
+      ext_modules=cythonize(ext_modules),
       include_dirs = [np.get_include()],
       long_description=open("README.md").read()
 )
