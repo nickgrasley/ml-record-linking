3be9610987f99d5a904c9a88b51659a866a3de83
Ben Busath <bbusath5@gmail.com>
2020-01-01 23:56:08 -0700

    added non-cython py files for testing

---
+++ Splycer-Dev/feature_engineer.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# cython: profile=True
+"""
+Created on Mon Mar  4 16:54:36 2019
+
+@author: ngrasley
+"""
+import warnings
+import copy
+import json
+from collections import OrderedDict
+from ast import literal_eval
+import numpy as np
+from splycer.base import FeatureBase
+from splycer.comparisons import JW, EuclideanDistance, GeoDistance, BiGram, TriGram, NGram,\
+                        BooleanMatch, AbsDistance
+
+class FeatureEngineer(FeatureBase):
+    """This class generates an array of similarity/distance scores by building
+       a pipeline of comparisons. Currently, it crucially assumes that the two
+       record sets that you are comparing have similarly named columns.
+    """
+    def __init__(self):
+        self.compares_avail = {"jw": JW(),
+                               "abs dist": AbsDistance(),
+                               "euclidean dist": EuclideanDistance(),
+                               "geo dist": GeoDistance(),
+                               "bigram": BiGram(),
+                               "trigram": TriGram(),
+                               "ngram": NGram(),
+                               "exact match": BooleanMatch()}
+        self.raw_compares = []
+        self.rec_columns = set()
+        self.ncompares = 0
+        self.pipeline = []
+
+    def show_available_compares(self):
+        """Output the currently implemented comparisons to the console."""
+        print(self.compares_avail)
+    
+    def add_comparison(self, record_col, compare_type, extra_args=None):
+        """Add a similarity/distance score to the feature engineer.
+           use show_available_compares() to show which compares are implemented.
+           Parameters:
+               record_col (string or tuple): the column(s) that you wish to compare.
+               compare_type (string): the comparison you want to do on the columns.
+                                      the name corresponds to the key in self.compares_avail.
+               extra_args (dict): optional. Pass additional arguments to a comparison,
+                                  such as commonality weights.
+           Returns:
+               Nothing. Updates pipeline with comparison.
+        """
+        if extra_args is None:
+            extra_args = {}
+        self.raw_compares.append([record_col, compare_type, extra_args])
+        if type(record_col) == list:
+            for i in record_col:
+                self.rec_columns.add(i)
+        elif type(record_col) == str:
+            self.rec_columns.add(record_col)
+        else:
+            raise TypeError("record column is neither a string nor a list")
+        self.ncompares += 1
+        try:
+            comp_func = copy.deepcopy(self.compares_avail[compare_type])
+        except KeyError:
+            print(f"{compare_type} is not a currently implemented comparison")
+        comp_func.__init__(**extra_args)
+        if type(record_col) == str or (type(record_col) == list and len(record_col) == 1):
+            comp_func.col = record_col
+        else:
+            comp_func.col = list(record_col)
+        self.pipeline.append(comp_func)
+        
+    def rm_comparison(self, pipeline_index):
+        """Remove a comparison by its index in the pipeline list."""
+        self.pipeline.pop(pipeline_index)
+        self.raw_compares.pop(pipeline_index)
+        warnings.warn("rm_comparison does not update the rec_columns attribute.")
+        
+    def check_pipeline(self, example_rec):
+        """check_pipeline currently checks for two things:
+           1. It warns the user if some columns from the example record array
+              are not being used. Some columns will obviously not want a comparison
+              (e.g. commonality weights), but this will catch any major columns
+              that the user will likely want to compare.
+           2. It raises an exception if there is a comparison done on a column
+              that is not in the example record array. You cannot run the pipeline
+              with this type of error.
+           check_pipeline should be run on both records sets to ensure that both
+           have the correct column names.
+        """
+        unused_cols = set(example_rec.dtype.names).difference(self.rec_columns)
+        if len(unused_cols) > 0:
+            warnings.warn(f"The column(s) {unused_cols} do(es) not have a \
+                              comparison function.", RuntimeWarning)
+            
+        extra_cols = self.rec_columns.difference(set(example_rec.dtype.names))
+        if len(extra_cols) > 0:
+            raise Exception(f"The column(s) {extra_cols} are not in the \
+                                example record. Please remove the offending \
+                                columns and run check_pipeline again.")
+
+    def compare(self, rec1, rec2):
+        """Run the comparison pipeline on the two given records.
+           Returns:
+               A float32 numpy array of the pipeline's comparison scores.
+        """
+        comp_vec = np.ndarray((rec1.shape[0],self.ncompares), dtype=np.float32)
+        i = 0
+        for compare in self.pipeline:
+            comp_vec[:,i] = compare.transform(rec1, rec2)
+            i += 1
+        return comp_vec
+
+    def save(self, file_path):
+        """Save the feature engineer at the specified file path."""
+        with open(file_path, 'w') as f:
+            for comp in self.raw_compares:
+                f.write(f"comp[0],comp[1],comp[2]\n")
+
+    def load(self, file_path):
+        """Load the feature engineer from the specified file path."""
+        self.__init__()
+        with open(file_path, 'r') as f:
+            file_str = f.readline()
+            while file_str:
+                comp = file_str.split(',')
+                self.add_comparison(comp[0], comp[1], comp[2])
+                file_str.readline()
---
+++ Splycer-Dev/record_set.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# cython: profile=True
+"""
+Created on Mon Jul 22 14:02:20 2019
+
+@author: Nick Grasley (ngrasley@stanford.edu)
+"""
+import numpy as np
+import pandas as pd
+import turbodbc
+from base import RecordBase
+
+class RecordDict(dict, RecordBase): #FIXME update the output format of data from this class. It should be a Pandas DataFrame.
+    """Records are organized in a dictionary with the key as a unique identifier
+       and the value as a numpy structured array of record info. Since dictionary
+       lookup scales at a constant rate with the number of records, this object
+       is most efficient when you merely have to grab record information. This
+       assumes the record arrays are numpy structured arrays.
+    """
+    def __init__(self, record_id, uids, features):
+        self.record_id = record_id
+        self.var_list = None
+        super().__init__(zip(uids, features))
+    
+    def set_var_list(self, var_list):
+        self.var_list = var_list
+    
+    def get_record(self, uid):
+        if self.var_list is None:
+            return pd.DataFrame.from_records(self.get(uid))
+        return pd.DataFrame.from_records(self.get(uid)[self.var_list])
+
+    def get_records(self, uids):
+        rec_array = np.concatenate(tuple(self.get(i) for i in uids))
+        if self.var_list is None:
+            return pd.DataFrame.from_records(rec_array)
+        return pd.DataFrame.from_records(rec_array)[self.var_list]
+
+class RecordDB(RecordBase): #FIXME this class assumes a standardized naming convention for all comparable record sets, i.e. I need to remove years from variable names.
+    """Records are stored in a sql database. If you need to do any blocking,
+       sql handles a lot of the hard work of building data structures for efficient
+       merges. You have to pay the upfront cost of setting up the database though.
+    """
+    def __init__(self, record_id, table_name, idx_name, dsn, extra_joins=''):
+        self.record_id = record_id
+        self.var_list = None
+        self.table_name = table_name
+        self.idx_name = idx_name
+        options = turbodbc.make_options(prefer_unicode=True) #Apparently necessary for MS sql servers.
+        self.conn = turbodbc.connect(dsn=dsn, turbodbc_options=options)
+        self.cursor = self.conn.cursor()
+        self.extra_joins = extra_joins
+        """
+        self.cursor.execute(f"select column_name from information_schema.columns where table_name = '{self.table_name}'")
+        cols = self.cursor.fetchall()
+        self.cols = np.ndarray(len(cols), dtype="U50")
+        for i in range(len(cols)):
+            self.cols[i] = cols[i][0]
+        """
+
+    def set_var_list(self, var_list):
+        self.var_list = var_list
+        
+    def set_joins(self, join_str):
+        self.extra_joins = join_str
+
+    def __getitem__(self, uid):
+        return self.get_record(uid)
+
+    def get_record(self, uid):
+        if self.var_list is None:
+            data = pd.read_sql(f"select * from {self.table_name} where {self.idx_name} = {uid} {self.extra_joins}", self.conn)
+        else:
+            data = pd.read_sql(f"select {self.var_list} from {self.table_name} where {self.idx_name} = {uid} {self.extra_joins}", self.conn)
+        return data
+#FIXME return records from get_records in same order as uid list
+    def get_records(self, uids):
+        if self.var_list is None:
+            data = pd.read_sql(f"select * from {self.table_name} where {self.idx_name} in {tuple(uids)} {self.extra_joins}", self.conn)
+        else:
+            data = pd.read_sql(f"select {self.var_list} from {self.table_name} where {self.idx_name} in {tuple(uids)} {self.extra_joins}", self.conn)
+        return data.set_index('index').loc[uids].reset_index().rename(columns={'index':self.idx_name})
+
+class RecordDataFrame(RecordBase):
+    """Records are stored in a Pandas DataFrame. This is best for small datasets
+       with a limited number of string features, or if you need to block and don't
+       want to set up a sql server. However, it has slow lookup, slow merges, and
+       is a memory hog, so don't use this for large datasets.
+    """
+    def __init__(self, record_id, records, uid_col=None):
+        self.record_id = record_id
+        self.var_list = None
+        if type(records) == pd.core.frame.DataFrame:
+            self.df = records
+        else:
+            self.df = pd.DataFrame(records, index=uid_col)
+
+    def set_var_list(self, var_list):
+        self.var_list = var_list
+
+    def __getitem__(self, uid):
+        return self.df.loc[uid, :]
+
+    def get_record(self, uid):
+        if self.var_list is None:
+            return self.df.loc[[uid], :].reset_index(drop=True)
+        return self.df.loc[[uid], self.var_list].reset_index(drop=True)
+
+    def get_records(self, uids):
+        if self.var_list is None:
+            return self.df.loc[uids, :].reset_index(drop=True)
+        return self.df.loc[uids, self.var_list].reset_index(drop=True)
+
