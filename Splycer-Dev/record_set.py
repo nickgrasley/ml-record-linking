320bed3f5df43b672a6685ff1db2255b77bb1267
Benjamin Busath <bbusath5@byu.edu>
2020-01-16 15:06:14 -0700

    added chunksize feature to record_db

--- Splycer-Dev/record_set.py
+++ Splycer-Dev/record_set.py
@@ -10,6 +10,8 @@ import numpy as np
 import pandas as pd
 import turbodbc
 from base import RecordBase
+import itertools
+from tqdm import tqdm
 
 class RecordDict(dict, RecordBase): #FIXME update the output format of data from this class. It should be a Pandas DataFrame.
     """Records are organized in a dictionary with the key as a unique identifier
@@ -42,7 +44,7 @@ class RecordDB(RecordBase): #FIXME this class assumes a standardized naming conv
        sql handles a lot of the hard work of building data structures for efficient
        merges. You have to pay the upfront cost of setting up the database though.
     """
-    def __init__(self, record_id, table_name, idx_name, dsn, extra_joins=''):
+    def __init__(self, record_id, table_name, idx_name, dsn, extra_joins='',chunksize=10000):
         self.record_id = record_id
         self.var_list = None
         self.table_name = table_name
@@ -51,6 +53,7 @@ class RecordDB(RecordBase): #FIXME this class assumes a standardized naming conv
         self.conn = turbodbc.connect(dsn=dsn, turbodbc_options=options)
         self.cursor = self.conn.cursor()
         self.extra_joins = extra_joins
+        self.chunksize = chunksize
         """
         self.cursor.execute(f"select column_name from information_schema.columns where table_name = '{self.table_name}'")
         cols = self.cursor.fetchall()
@@ -67,21 +70,32 @@ class RecordDB(RecordBase): #FIXME this class assumes a standardized naming conv
 
     def __getitem__(self, uid):
         return self.get_record(uid)
-
+               
     def get_record(self, uid):
         if self.var_list is None:
             data = pd.read_sql(f"select * from {self.table_name} where {self.idx_name} = {uid} {self.extra_joins}", self.conn)
         else:
             data = pd.read_sql(f"select {self.var_list} from {self.table_name} where {self.idx_name} = {uid} {self.extra_joins}", self.conn)
         return data
-#FIXME return records from get_records in same order as uid list
+        
+    def chunks(self, l, n):
+    # divides a list into n-sized chunks. Used for get_records in record_db class
+    # to read in SQL data chunk wise-- this prevents overly complex SQL queries
+        for i in range(0, len(l), n):
+            # Create an index range for l of n items:
+            yield l[i:i+n]
+          
     def get_records(self, uids):
-        if self.var_list is None:
-            data = pd.read_sql(f"select * from {self.table_name} where {self.idx_name} in {tuple(uids)} {self.extra_joins}", self.conn)
-        else:
-            data = pd.read_sql(f"select {self.var_list} from {self.table_name} where {self.idx_name} in {tuple(uids)} {self.extra_joins}", self.conn)
+        data_frames=[]
+        for chunk_uids in tqdm(self.chunks(uids,self.chunksize)):
+            if self.var_list is None:
+                data_frames.append(pd.read_sql(f"select * from {self.table_name} where {self.idx_name} in {tuple(chunk_uids)} {self.extra_joins}", self.conn))
+            else:
+                data_frames.append(pd.read_sql(f"select {self.var_list} from {self.table_name} where {self.idx_name} in {tuple(uids)} {self.extra_joins}", self.conn))
+        data=pd.concat(data_frames).drop_duplicates()
         return data.set_index('index').loc[uids].reset_index().rename(columns={'index':self.idx_name})
-
+    
+ 
 class RecordDataFrame(RecordBase):
     """Records are stored in a Pandas DataFrame. This is best for small datasets
        with a limited number of string features, or if you need to block and don't
